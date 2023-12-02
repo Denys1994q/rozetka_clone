4fab77289a85cce623642779bb2f0f7343c207a3
Denys1994q <drybachok@gmail.com>
2023-12-02 14:19:50 +0200

    fix ssr on vercel, change build command on vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,6 +35,7 @@ export class AppComponent implements OnInit {
     showHeader: boolean = true
 
     ngOnInit(): void {
+        console.log(12)
         this.authService.getUser().subscribe({
             next: user => this.wishlistService.setWishlistItems(user.wishlist),
             error: (error) => {
